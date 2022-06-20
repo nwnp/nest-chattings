c76efd388b609e39a653377ea20fd78da388b87a
nwnp <jjw_0418@naver.com>
2022-06-20 23:35:03 +0900

    feat: Chatting room 완성

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,4 +1,5 @@
 import { Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import {
   ConnectedSocket,
   MessageBody,
@@ -6,35 +7,74 @@ import {
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import { Model } from 'mongoose';
 import { Socket } from 'socket.io';
+import { Socket as SocketModel } from './models/sockets.model';
+import { Chatting } from './models/chattings.model';
 
 @WebSocketGateway({ namespace: 'chattings' })
 export class ChatsGateway implements OnGatewayConnection {
   private logger = new Logger('chat');
 
+  constructor(
+    @InjectModel(Chatting.name) private readonly chattingModel: Model<Chatting>,
+    @InjectModel(SocketModel.name)
+    private readonly socketModel: Model<SocketModel>,
+  ) {}
+
   /** 연결이 끊기는 순간 실행되는 함수 */
-  handleDisconnect(@ConnectedSocket() socket: Socket) {
+  async handleDisconnect(@ConnectedSocket() socket: Socket) {
+    const user = await this.socketModel.findOne({ id: socket.id });
+    if (user) {
+      socket.broadcast.emit('disconnect_user', user.username);
+      await user.delete();
+    }
     this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
   }
 
   /** 연결되는 순간 실행되는 함수 */
-  @SubscribeMessage('new_user')
   handleConnection(
     @ConnectedSocket()
     socket: Socket,
-    @MessageBody() username: string,
   ) {
-    this.logger.log(
-      `connected with ${username}: ${socket.id} ${socket.nsp.name}`,
-    );
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
   }
 
   @SubscribeMessage('new_user')
-  handleNewUser(
+  async handleNewUser(
     @MessageBody() username: string,
     @ConnectedSocket() socket: Socket, // emit or on을 할 수 있음
   ) {
+    const exist = await this.socketModel.exists({ username });
+    if (exist) {
+      username = `${username}_${Math.floor(Math.random() * 100)}`;
+      await this.socketModel.create({
+        id: socket.id,
+        username,
+      });
+    } else {
+      await this.socketModel.create({
+        id: socket.id,
+        username,
+      });
+    }
     socket.broadcast.emit('user_connected', username); // broadcasting
     return username;
+  }
+
+  @SubscribeMessage('submit_chat')
+  async handleSubmitChat(
+    @MessageBody() chat: string,
+    @ConnectedSocket() socket: Socket, // emit or on을 할 수 있음
+  ) {
+    const socketObj = await this.socketModel.findOne({ id: socket.id });
+    await this.chattingModel.create({
+      user: socketObj,
+      chat: chat,
+    });
+    socket.broadcast.emit('new_chat', {
+      chat,
+      username: socketObj.username,
+    }); // broadcasting
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function main() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -10,6 +11,7 @@ async function main() {
   app.useStaticAssets(join(__dirname, '../public')); // css 파일 경로 setting
   app.setBaseViewsDir(join(__dirname, '../views')); // html 파일 경로 setting
   app.setViewEngine('hbs');
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT);
 }
