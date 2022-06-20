20ccb2d93cc0bce843215679b9b7362fa27de3b4
nwnp <jjw_0418@naver.com>
2022-06-20 19:54:59 +0900

    feat: Add broadcasting

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,16 +1,23 @@
-const socket = io('/');
+const socket = io('/chattings');
 const getElementId = (id) => document.getElementById(id);
 
 const helloStrangerElement = getElementId('hello_stranger');
 const chattingBoxElement = getElementId('chatting_box');
 const formElement = getElementId('chat_form');
 
+socket.on('user_connected', (username) => {
+  console.log(`${username} connected`);
+});
+
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `Hello ${username} stranger :) `);
+
 function helloUser() {
   const username = prompt('What is your name?');
-  socket.emit('new_user', username);
-  socket.on('hello_user', (data) => {
-    console.log(data);
+  socket.emit('new_user', username, (data) => {
+    drawHelloStranger(data);
   });
+  socket.on('hello_user');
 }
 
 function init() {
--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,20 +1,40 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway implements OnGatewayConnection {
+  private logger = new Logger('chat');
+
+  /** 연결이 끊기는 순간 실행되는 함수 */
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  /** 연결되는 순간 실행되는 함수 */
+  @SubscribeMessage('new_user')
+  handleConnection(
+    @ConnectedSocket()
+    socket: Socket,
+    @MessageBody() username: string,
+  ) {
+    this.logger.log(
+      `connected with ${username}: ${socket.id} ${socket.nsp.name}`,
+    );
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
     @ConnectedSocket() socket: Socket, // emit or on을 할 수 있음
   ) {
-    console.log(socket.id);
-    console.log(username);
-    socket.emit('hello_user', 'hello ' + username);
+    socket.broadcast.emit('user_connected', username); // broadcasting
+    return username;
   }
 }
